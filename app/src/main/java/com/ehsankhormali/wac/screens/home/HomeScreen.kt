0918ee40c8e5f929f82531daa805b99101339af2
Ehsan <ehsankhormali@gmail.com>
2023-06-04 23:58:26 +0330

    author of post added to card

--- app/src/main/java/com/ehsankhormali/wac/screens/home/HomeScreen.kt
+++ app/src/main/java/com/ehsankhormali/wac/screens/home/HomeScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
@@ -23,12 +24,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
 import com.ehsankhormali.wac.R
 import com.ehsankhormali.wac.data.RequestState
 
@@ -62,20 +65,34 @@ fun HomeScreen(navController: NavController, viewModel: HomeScreenViewModel) {
                                                 Text(text = postItem.excerpt.rendered)
                                             }
                                         }
-                                        Divider(thickness = 2.dp)
+                                        Divider(thickness = 1.dp)
                                         Row(modifier = Modifier
                                             .height(45.dp)
                                             .fillMaxWidth(),
-                                        horizontalArrangement = Arrangement.End) {
+                                        horizontalArrangement = Arrangement.SpaceAround) {
                                             IconButton(onClick = { /*TODO*/ }) {
                                                 Icon(
                                                     painter = painterResource(id = R.drawable.chat_bubble_fill0_wght400_grad0_opsz48),
                                                     contentDescription ="",
-                                                    modifier = Modifier.size(35.dp)
+                                                    modifier = Modifier.size(24.dp)
                                                 )
                                                 Text(text = (postItem.embedded.replies?.get(0)?.size?:0).toString(),
                                                     style = MaterialTheme.typography.labelSmall
                                                 )
+                                            }
+
+                                            Row(modifier = Modifier.padding(5.dp),
+                                            verticalAlignment = Alignment.CenterVertically) {
+                                                AsyncImage(
+                                                    model = postItem.embedded.author[0].avatarUrls.size24,
+                                                    placeholder = painterResource(id = R.drawable.image_placeholder),
+                                                    error = painterResource(id = R.drawable.image_placeholder),
+                                                    contentDescription = null,
+                                                    modifier = Modifier.size(24.dp)
+                                                        .clip(shape = CircleShape)
+                                                )
+                                                Text(text = postItem.embedded.author[0].name,
+                                                    style = MaterialTheme.typography.labelSmall)
                                             }
                                         }
                                     }
