134aed408f2d215e159548d6c8131f4e878f545e
Ehsan <ehsankhormali@gmail.com>
2023-06-11 03:35:33 +0330

    added profile Icon to MainAppBar

--- app/src/main/java/com/ehsankhormali/wac/components/appbar/MainAppBar.kt
+++ app/src/main/java/com/ehsankhormali/wac/components/appbar/MainAppBar.kt
@@ -7,10 +7,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -21,20 +23,25 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import coil.compose.rememberAsyncImagePainter
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainAppBar(query:String,navController: NavController) {
+fun MainAppBar(query: String, navController: NavController) {
     var text by rememberSaveable { mutableStateOf(query) }
     var active by rememberSaveable { mutableStateOf(false) }
     Row(
-        modifier = Modifier.padding(4.dp),
-        //verticalAlignment = Alignment.CenterVertically
+        modifier = Modifier
+            .padding(4.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-
         SearchBar(
             query = text,
             onQueryChange = { text = it },
@@ -43,8 +50,22 @@ fun MainAppBar(query:String,navController: NavController) {
             onActiveChange = { active = it },
             placeholder = { Text(text = "Search") },
             leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = "") },
-            shape = RoundedCornerShape(15.dp),
-            modifier = Modifier.fillMaxWidth()
+            trailingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        painter = rememberAsyncImagePainter(
+                            model = "",
+                            error = rememberVectorPainter(image = Icons.Default.Person)
+                        ),
+                        contentDescription =""
+                    )
+                }
+                           },
+            shape = CircleShape,
+            modifier = Modifier
+                .padding(5.dp)
+                .weight(weight = 1f, fill = true)
+
         ) {
             LazyColumn(
                 modifier = Modifier.fillMaxWidth(),
@@ -66,9 +87,34 @@ fun MainAppBar(query:String,navController: NavController) {
             }
         }
 
+
+        /*if (!active) {
+            IconButton(
+                onClick = { *//*TODO*//* },
+                modifier = Modifier
+                    .padding(5.dp)
+                    .size(55.dp)
+                    .background(Color.Cyan)
+            ) {
+                AsyncImage(
+                    model = "",
+                    contentDescription = "profile picture",
+                    error = rememberVectorPainter(image = Icons.Default.Person)
+
+                )
+            }
+        }*/
+
+
     }
 }
 
-fun searchContent(query: String){
+fun searchContent(query: String) {
 
+}
+
+@Composable
+@Preview
+fun MainAppBarPreview() {
+    MainAppBar(query = "", navController = rememberNavController())
 }
